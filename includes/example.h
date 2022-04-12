933ad925487419f5f5105e53272e5cf568fc12dd
viruskizz <kizzaraiva@gmail.com>
2022-04-13 01:31:24 +0700

    add example

---
+++ includes/example.h
@@ -0,0 +1,46 @@
+#ifndef EXAMPLE_H
+# define EXAMPLE_H
+
+# include <math.h>
+# include "mlx.h"
+
+/*
+ Defines for the width and height of your window. I suggest you to do the same so
+ you can change easily the size of your window later if needed.
+*/
+# define WIN_WIDTH 800
+# define WIN_HEIGHT 600
+
+/*
+ Here I built a struct of the MLX image :
+ It contains everything you need.
+ - img_ptr to store the return value of mlx_new_image
+ - data to store the return value of mlx_get_data_addr
+ - the 3 other variables are pretty much useless, but you'll need
+ them in the prototype of mlx_get_data_addr (see the man page for that)
+ */
+typedef struct	s_img
+{
+	void		*img_ptr;
+	int			*data; //Here you got an int * even though mlx_get_data_addr returns
+						//a char *, i'll talk more about this in the .c file.
+//Here are the 3 "useless" variables. You can find more informations about these in the man page.
+	int			size_l;
+	int			bpp;
+	int			endian;
+}				t_img;
+
+/*
+ Here is my main struct containing every variables needed by the MLX.
+ - mlx_ptr stores the return value of mlx_init
+ - win stores the return value of mlx_new_window
+ - img will store everything we need for the image part, the struct is described above.
+ */
+typedef struct	s_mlx
+{
+	void		*mlx_ptr;
+	void		*win;
+	t_img		img;
+}				t_mlx;
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,6 @@
+#include "example.h"
 #include "libft.h"
 #include <stdio.h>
-#include <mlx.h>
 
 int	main(void)
 {
@@ -12,8 +12,57 @@ int	main(void)
 	printf("Hello MLX\n");
 	ft_putstr_fd("TEST Libft\n", 1);
 
-	void *mlx = mlx_init();
-	void *win = mlx_new_window(mlx, 640, 360, "Tutorial Window");
-	printf("win %p\n", win);
-    mlx_loop(mlx);
+	// Minilibx Example
+	t_mlx	mlx; //Here I first create my struct that will contains all the "MLX stuff"
+	int		count_w;
+	int		count_h;
+
+	count_h = -1;
+	//First you need to call mlx_init and store its return value.
+	mlx.mlx_ptr = mlx_init();
+	//Now do the same with mlx_new_window
+	mlx.win = mlx_new_window(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "A simple example");
+	//One more time with mlx_new_image
+	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
+	/*
+	 Now the important part :
+	 mlx_get_data_addr will return a char* that is 4 times the (width * height) of your image.
+	 Why so ? Let me explain : This char* will represent your image, pixel by pixel,
+	 and the values of this array are the colors. That's why the array is 4 times bigger :
+	 you need 4 char to code the color of each pixels (one for Red, Green and Blue) and one for the alpha.
+	 But... it's not very convenient, right ? So here is my little trick : you cast
+	 mlx_get_data_addr as an int* and store it in an int*.
+	 This way, the array will have the exact same size as your window, and each index
+	 will represent one complete color of a pixel !
+	*/
+	mlx.img.data = (int *)mlx_get_data_addr(mlx.img.img_ptr, &mlx.img.bpp, &mlx.img.size_l,
+		&mlx.img.endian);
+	/*
+	 Now just a little example : here is a loop that will draw each pixels that
+	 have an odd width in white and the ones that have an even width in black.
+	*/
+	while (++count_h < WIN_HEIGHT)
+	{
+		count_w = -1;
+		while (++count_w < WIN_WIDTH)
+		{
+			if (count_w % 2)
+				/*
+				 As you can see here instead of using the mlx_put_pixel function
+				 I just assign a color to each pixel one by one in the image,
+				 and the image will be printed in one time at the end of the loop.
+				 Now one thing to understand here is that you're working on a 1-dimensional
+				 array, while your window is (obviously) 2-dimensional.
+				 So, instead of having data[height][width] here you'll have the following
+				 formula : [current height * max width + current width] (as you can see below)
+				*/
+				mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFFFFFF;
+			else
+				mlx.img.data[count_h * WIN_WIDTH + count_w] = 0;
+		}
+	}
+	//Now you just have to print the image using mlx_put_image_to_window !
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img.img_ptr, 0, 0);
+	mlx_loop(mlx.mlx_ptr);
+	return (0);
 }
